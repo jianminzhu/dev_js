06529b152e2ea7759c657c55dd4a69233814ec4c
zhujianmin <zhujianmin@sogou-inc.com>
2017-06-13 10:41:23 +0800

    ny

--- ny_data/SDataUtilTest.js
+++ ny_data/SDataUtilTest.js
@@ -10,6 +10,6 @@ var dbconfig = {
 var sDataUtil = new SDataUtil_1.SDataUtil(dbconfig);
 var startPage = 1;
 var endPage = 183;
-var jys = "IPE", pz = "OIL";
+var jys = "NYME", pz = "CL";
 var t = "ps_d_" + jys.toLowerCase() + "_" + pz.toLowerCase();
 sDataUtil.spider(jys, pz, startPage, endPage);
--- ny_data/SDataUtilTest.ts
+++ ny_data/SDataUtilTest.ts
@@ -10,6 +10,6 @@ let dbconfig = {
 let sDataUtil = new SDataUtil(dbconfig);
 var startPage = 1
 var endPage = 183
-var jys = "IPE", pz = "OIL"
+var jys = "NYME", pz = "CL";
 var t = `ps_d_${jys.toLowerCase()}_${pz.toLowerCase()}`;
 sDataUtil.spider(jys, pz, startPage, endPage)
